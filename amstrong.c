05d1fde81c90235ca658457b4336eeaec81d085f
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-01-21 11:18:19 +0530

    Create amstrong.c

---
+++ amstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,i,num;
+printf("enter the two numbers(intervals)");
+scanf("%d%d"&n1,&n2);
+printf("amstrong numbers between %d and %d are:"n1,n2);
+for(i=n1+1;i<n2;i++)
+{
+num=0;
+}
+if(i==num)
+{
+printf("%d",i);
+}
+else
+retun 0;
+}
