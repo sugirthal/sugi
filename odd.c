7a2e6586cf241f7aa5aea19e28b1144bb7d5983d
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-01-21 11:56:52 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -2,14 +2,10 @@
 void main()
 {
 int num,m =20,n=40;
-clrscr();
 printf("Print Odd Numbers in a given range m to n:\n");
 for (num = m; num <= n; num++)
-{
 if (num % 2 == 1)
 {
 printf ("%d ", num);
-}
-getch();
 }
 }
