19c1e0d7a9d8217f48662f2b88ca13906c80f2e1
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-01-21 09:43:10 +0530

    Update notprime.c

--- notprime.c
+++ notprime.c
@@ -1,17 +1,17 @@
 main()
 {
-int a, b;
-printf("Enter the number");
-scanf("%d", &a);
-for(b=2;b<=n/2;b++;)
+int n,i;
+printf("Enter the integer to find prime or not");
+scanf("%d", &n);
+for(i=2;i<=n/2;i++)
 {
-if(a%b==0)
+if(n%i==0)
 {
 break;
 }
 }
-if(i>a/2)
-printf("%d the given number is prime",a);
+if(i>n/2)
+printf("%d the given number is prime",n);
 else
-printf("%d the given number is not prime",a);
+printf("%d the given number is not prime",n);
 }
