8d610e151aba7808237a46324807d2f7e85b77c6
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-01-21 11:37:26 +0530

    Update amstrong.c

--- amstrong.c
+++ amstrong.c
@@ -2,18 +2,18 @@
 int main()
 {
 int n1,n2,i,num,temp,rem;
-printf("enter the two numbers(intervals)");
+printf("enter the two numbers(intervals):");
 scanf("%d%d",&n1,&n2);
 printf("amstrong numbers between %d and %d are:,"n1,n2);
 for(i=n1+1;i<n2;i++)
 {
-temp=i;
-num=0;
-while(temp!=0)  
+  temp=i;
+  num=0;
+  while(temp!=0)  
 {
-rem=(temp%10);
-num=rem*rem*rem;
-temp/=10;
+  rem=(temp%10);
+  num=rem*rem*rem;
+  temp/=10;
 }
 if(i==num)
 {
