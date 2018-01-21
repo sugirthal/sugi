d6f7fe2baa31d5081a3986fb2d08c2de4c0f58b2
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-01-21 09:35:10 +0530

    Create notprime.c

---
+++ notprime.c
@@ -0,0 +1,17 @@
+main()
+{
+int a, b;
+printf("Enter the number");
+scanf("%d", &a);
+for(b=2;b<=n/2;b++;)
+{
+if(a%b==0)
+{
+break;
+}
+}
+if(i>a/2)
+printf("%d the given number is prime",a);
+else
+printf("%d the given number is not prime",a);
+}
