e3703aedae6743dce6ff1e0d82522e182841a8f5
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-01-23 16:08:14 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main();
+{
+int i,n;
+printf("print all even numbers till:");
+scanf("%d",n);
+printf("all even numbers from 1 to %d are\n:",n);
+i=1;
+while(i<=n)
+{
+if(i%2==0)
+{
+printf("%d\n",n);
+}
+i++;
+}
+return 0;
+}
