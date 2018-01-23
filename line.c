85e617ef3bfe2c45d940fd14b221f6931a66b1e6
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-01-23 16:50:08 +0530

    Update line.c

--- line.c
+++ line.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 void main()
 {
-     char s[50],ch;
-     int i,c=0;
-     printf("Enter any string : ");
-     for(i=0;ch!='\n';i++)
-     {
-     ch=getchar();
-     s[i]=ch;
-     }
-     s[i]='\0';
-
-     for(i=0;s[i]!='\0';i++){
-    if(s[i]==' '){
-       c++;
-       while(s[i]==' ')
-        i++;
-    }
-     }
-     printf("\n\nTotal words are %d",c+1);
+char s[50],ch;
+int i,c=0;
+printf("Enter any string : ");
+for(i=0;ch!='\n';i++)
+{
+ch=getchar();
+s[i]=ch;
+ }
+ s[i]='\0';
+for(i=0;s[i]!='\0';i++){
+ if(s[i]==' ')
+ {
+ c++;
+ while(s[i]==' ')
+ i++;
+ }
+ printf("\n\nTotal words are %d",c+1);
 }
