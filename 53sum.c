ad555e8f4902d9780dbf8f91c5c7e07170a27b68
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-02-06 09:36:12 +0000

    Update 53sum.c

--- 53sum.c
+++ 53sum.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-int main()
+void main()
 {
 int num,sum,r;
-printf("the num is : \n");
+printf("the num is \n");
 scanf("%d",&num);
 while(num>0)
 {
-r=n%10;
+r=num%10;
 sum=sum+r;
 num=num/10;
-printf("%d",r);
 }
-return 0;
+printf("%d",sum);
 }
