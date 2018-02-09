ad3613a39c082568a1edc8721d848128f5208bdd
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-02-09 09:14:27 +0000

    Update 66prime.c

--- 66prime.c
+++ 66prime.c
@@ -1,6 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int n;
-printf(" ");
-scanf("%d",&n);
+int i, num, p = 0;
+printf("Please enter a number: \n");
+scanf("%d", &num);
+for(i=1; i<=num; i++)
+{
+if(num%i==0)
+{
+ p++;
+ }
+ }
+ if(p==2)
+ {
+ printf("yes");
+   }
+ else
+ {
+  printf("no");
+   }
+}
