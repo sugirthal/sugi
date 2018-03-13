616d81d3cca41f941b2bb7232aa025f5d6316eaa
sugirthal <31504774+sugirthal@users.noreply.github.com>
2018-03-13 17:00:06 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s1[50],s2[50];
+int l1,l2;
+printf("enter the string: ");
+scanf("%s",s1);
+printf("enter the string:");
+scanf("%s",s2);
+l1=strlen(s1);
+l2=strlen(s2);
+if(l1==l2)
+{
+  printf("%s",s1);
+}
+else if(l1>l2)
+{
+printf("%s",s1);
+}
+else
+{
+  printf("%s",s2);
+}
+return 0;
+}
